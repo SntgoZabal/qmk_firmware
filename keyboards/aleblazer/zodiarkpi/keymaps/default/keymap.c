610a0e3c6d823933dd6bd6208cf206fa353c971d
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-06-06 22:52:22 -0600

    Update keymap.c

--- keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
@@ -17,9 +17,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
       KC_ESC,    KC_1,     KC_2,      KC_3,      KC_4,       KC_5,                                                             KC_6,       KC_7,        KC_8,        KC_9,       KC_0,       KC_BSPC,
-      SCR_LOCK,  KC_Q,     KC_W,      KC_E,      KC_R,       KC_T,                                                             KC_Y,       KC_U,        KC_I,        KC_O,       KC_P,       KC_LBRC,
+      KC_SLEP,   KC_Q,     KC_W,      KC_E,      KC_R,       KC_T,                                                             KC_Y,       KC_U,        KC_I,        KC_O,       KC_P,       KC_LBRC,
       KC_PSCR,   KC_A,     KC_S,      KC_D,      KC_F,       KC_G,     KC_RSFT,    KC_MUTE,          RGB_TOG,     KC_ENT,      KC_H,       KC_J,        KC_K,        KC_L,       KC_SCLN,    KC_RBRC,
-      KC_LSFT,   KC_Z,     KC_X,      KC_C,      KC_V,       KC_B,     KC_LCTL,    KC_LALT,          KC_EQL,      KC_MINS,     KC_N,       KC_M,        KC_COMM,     KC_DOT,     KC_SLSH,    KC_QUOT ,
+      KC_LSFT,   KC_Z,     KC_X,      KC_C,      KC_V,       KC_B,     KC_LCTL,    KC_LALT,          KC_EQL,      KC_MINS,     KC_N,       KC_M,        KC_COMM,     KC_DOT,     KC_SLSH,    KC_QUOT,
       TO(0),     TO(1),    TO(2),     TO(3),     KC_GRV,          KC_TAB,          KC_MPLY,          KC_ENT,            KC_SPC,            KC_LEFT,     KC_UP,       KC_DOWN,    KC_RGHT,    KC_BSLS
     ),
 
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_PSLS,   KC_P7,    KC_P8,     KC_P9,     _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F12,   
       KC_CAPS,   KC_P4,    KC_P5,     KC_P6,     KC_PDOT,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    _______,
       KC_P0,     KC_P1,    KC_P2,     KC_P3,     KC_PENT,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
-      TO(0),     TO(1),    TO(2),     TO(3),     _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______     _______,
+      TO(0),     TO(1),    TO(2),     TO(3),     _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 
 	[2] = LAYOUT(
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_PSLS,   KC_P7,    KC_P8,     KC_P9,     _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F12,   
       KC_CAPS,   KC_P4,    KC_P5,     KC_P6,     KC_PDOT,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    _______,
       KC_P0,     KC_P1,    KC_P2,     KC_P3,     KC_PENT,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
-      TO(0),     TO(1),    TO(2),     TO(3),     _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______     _______,
+      TO(0),     TO(1),    TO(2),     TO(3),     _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 
 	[3] = LAYOUT(

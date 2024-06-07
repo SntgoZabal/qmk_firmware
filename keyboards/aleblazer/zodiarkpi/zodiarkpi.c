9e21ed51ec1c56478df3bf7d337f82baab39f347
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-06-06 20:55:50 -0600

    Update zodiarkpi.c

--- keyboards/aleblazer/zodiarkpi/zodiarkpi.c
+++ keyboards/aleblazer/zodiarkpi/zodiarkpi.c
@@ -136,9 +136,9 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     }
     if (index == 0) { /* First encoder */
         if (clockwise) {
-            tap_code_delay(KC_VOLD, 10);
-        } else {
             tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
         }
     } else if (index == 1) { /* Second encoder */
         if (clockwise) {

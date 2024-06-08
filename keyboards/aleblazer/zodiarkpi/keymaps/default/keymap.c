4394eb6c1cbc50cb15718792eb3251054a78c1d8
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-06-08 13:53:33 -0600

    Update keymap.c

--- keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case RGB_PRIMDN:
             if (record->event.pressed) {
                 // Switch to custom effect 2
-                rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_MY_COOL_EFFECT2);
+                rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_my_cool_effect2);
             }
             return false;
     }

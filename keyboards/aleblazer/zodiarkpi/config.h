dc1aa0b2c5e6f8da6cecb407a95beae9ee4cd3d4
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-06-08 01:50:21 -0600

    Update config.h

--- keyboards/aleblazer/zodiarkpi/config.h
+++ keyboards/aleblazer/zodiarkpi/config.h
@@ -51,6 +51,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
      /* Enable Framebuffer and keypress effects */
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_KEYPRESSES
+#define RGB_DISABLE_AFTER_TIMEOUT 0.5
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true
      /* Enable individiual effects */
 #define ENABLE_RGB_MATRIX_ALPHAS_MODS
 #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN

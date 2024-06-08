1050a21498edffc5c03a1176656c27a425d0cd1c
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-06-08 01:18:25 -0600

    Update keymap.c

--- keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/default/keymap.c
@@ -15,11 +15,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 void set_primary_color(uint8_t r, uint8_t g, uint8_t b) {
-    //rgblight_sethsv(r, g, b);
+    rgblight_sethsv(r, g, b);
 }
 
 void set_secondary_color(uint8_t r, uint8_t g, uint8_t b) {
-    //rgblight_sethsv_at(r, g, b); // Example: Set the secondary color at a specific index
+    rgblight_sethsv_at(r, g, b, 1); // Example: Set the secondary color at a specific index
 }
 
 enum custom_keycodes {
@@ -66,18 +66,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 	[1] = LAYOUT(
-      KC_NUM,    KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_BSPC,
-      KC_PSLS,   KC_P7,       KC_P8,        KC_P9,     _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
-      KC_CAPS,   KC_P4,       KC_P5,        KC_P6,     KC_PDOT,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
-      KC_P0,     KC_P1,       KC_P2,        KC_P3,     KC_PENT,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
+      KC_ESC,    KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_BSPC,
+      _______,   _______,     _______,      _______,   _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
+      KC_CAPS,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
+      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
       TO(0),     TO(1),       TO(2),        TO(3),     _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 
 	[2] = LAYOUT(
-      KC_NUM,    KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_BSPC,
-      KC_PSLS,   KC_P7,       KC_P8,        KC_P9,     _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
-      KC_CAPS,   KC_P4,       KC_P5,        KC_P6,     KC_PDOT,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
-      KC_P0,     KC_P1,       KC_P2,        KC_P3,     KC_PENT,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
+      KC_ESC,    KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_BSPC,
+      _______,   _______,     _______,      _______,   _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
+      KC_CAPS,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
+      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
       TO(0),     TO(1),       TO(2),        TO(3),     _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 

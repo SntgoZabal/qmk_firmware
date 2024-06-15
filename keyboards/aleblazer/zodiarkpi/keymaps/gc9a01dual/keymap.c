495ca115eb691202a96b6485bb2510ad05fa596e
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-06-14 19:47:34 -0600

    Update keymap.c

--- keyboards/aleblazer/zodiarkpi/keymaps/gc9a01dual/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/gc9a01dual/keymap.c
@@ -13,38 +13,124 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "unicode.h"
+
+enum custom_keycodes {
+    KB_MODE0 = SAFE_RANGE,
+    KB_MODE1,
+    KB_MODE2,
+    KB_MODE3,
+    KB_ACCENT
+};
+
+void keyboard_post_init_user(void) {
+    // Set the default RGB effect
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_SZV_EFFECT_0);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static bool accent_tilde_mode = false;
+    static uint16_t vowel_timer;
+
+    switch (keycode) {
+        case KB_MODE0:
+            if (record->event.pressed) {
+                // Switch to layer 0
+                layer_move(0);
+                // Switch to custom effect 0
+                rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_SZV_EFFECT_0);
+            }
+            return false;
+
+        case KB_MODE1:
+            if (record->event.pressed) {
+                // Switch to layer 1
+                layer_move(1);
+                // Switch to custom effect 1
+                rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_SZV_EFFECT_1);
+            }
+            return false;
+        
+        case KB_MODE2:
+            if (record->event.pressed) {
+                // Switch to layer 2
+                layer_move(2);
+                // Switch to custom effect 2
+                rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_SZV_EFFECT_2);
+            }
+            return false;
+        
+        case KB_MODE3:
+            if (record->event.pressed) {
+                // Switch to layer 3
+                layer_move(3);
+                // Switch to custom effect 3
+                rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_SZV_EFFECT_3);
+            }
+            return false;
+        
+        case KB_ACCENT:
+            if (record->event.pressed) {
+                if (get_mods() & MOD_BIT(KC_LSFT) || get_mods() & MOD_BIT(KC_RSFT)) {
+                    // Shift key is held, set accent_tilde_mode to true for tilde
+                    accent_tilde_mode = true;
+                } else {
+                    // Shift key is not held, set accent_tilde_mode to false for accent
+                    accent_tilde_mode = false;
+                }
+                vowel_timer = timer_read();
+            }
+            return false;
+        
+        default:
+            if (!record->event.pressed) {
+                if (timer_elapsed(vowel_timer) < TAPPING_TERM) {
+                    // Check if the key is a vowel and apply the accent/tilde
+                    if (accent_tilde_mode) {
+                        register_unicode(0x0303);
+                    } else {
+                        register_unicode(0x0301);
+                    }
+                }
+            }
+            break;
+        
+    }
+    return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[0] = LAYOUT(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-      MO(3), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,  KC_MINS, KC_MUTE,     RGB_TOG, KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV, KC_LBRC,      KC_RBRC, KC_DEL,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-      KC_LCTL, KC_LALT, KC_LGUI, KC_F12, MO(1),  KC_SPC,   KC_ENT,    KC_ENT,   KC_SPC,  MO(2),    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    [0] = LAYOUT(
+      KC_ESC,    KC_1,        KC_2,         KC_3,      KC_4,       KC_5,                                                             KC_6,       KC_7,        KC_8,        KC_9,       KC_0,       KC_BSPC,
+      KC_MPLY,   KC_Q,        KC_W,         KC_E,      KC_R,       KC_T,                                                             KC_Y,       KC_U,        KC_I,        KC_O,       KC_P,       KC_LBRC,
+      KC_PSCR,   KC_A,        KC_S,         KC_D,      KC_F,       KC_G,     KC_RSFT,    KC_MUTE,          RGB_TOG,     KC_ENT,      KC_H,       KC_J,        KC_K,        KC_L,       KC_SCLN,    KC_RBRC,
+      KC_LSFT,   KC_Z,        KC_X,         KC_C,      KC_V,       KC_B,     KC_LCTL,    KC_LALT,          KC_EQL,      KC_MINS,     KC_N,       KC_M,        KC_COMM,     KC_DOT,     KC_SLSH,    KC_QUOT,
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  KB_ACCENT,          KC_TAB,          KC_SLEP,          KC_LGUI,           KC_SPC,            KC_LEFT,     KC_UP,       KC_DOWN,    KC_RGHT,    KC_BSLS
     ),
 
 	[1] = LAYOUT(
-      _______, KC_F1, KC_F2, KC_F3, KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,  KC_F9, KC_F10, KC_F11,
-      KC_PSLS, KC_P7, KC_P8, KC_P9, _______, KC_NUM,                                      KC_NUM,  KC_PSLS, KC_P7, KC_P8, KC_P9, KC_F12,
-      KC_CAPS, KC_P4, KC_P5, KC_P6, _______, _______, _______, _______,  _______,  _______, _______, _______, KC_P4, KC_P5, KC_P6, _______,
-      _______, KC_P1, KC_P2, KC_P3, _______, _______, _______, _______,  _______, _______, _______, _______,  KC_P1, KC_P2, KC_P3, _______,
-      _______, KC_P0, KC_PDOT, KC_PENT, _______,     _______,  _______,   _______,    _______,   _______,     KC_P0, KC_PDOT, KC_PENT, _______
+      KC_ESC,    KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_BSPC,
+      _______,   _______,     _______,      _______,   _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
+      KC_CAPS,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
+      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 
 	[2] = LAYOUT(
-      _______, KC_F1, KC_F2, KC_F3, KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,  KC_F9, KC_F10, KC_F11,
-      KC_PSLS, KC_P7, KC_P8, KC_P9, _______, KC_NUM,                                      KC_NUM,   KC_PSLS, KC_P7, KC_P8, KC_P9, KC_F12,
-      KC_CAPS, KC_P4, KC_P5, KC_P6, _______, _______, _______, _______,  _______,  _______, _______, _______, KC_P4, KC_P5, KC_P6, _______,
-      _______, KC_P1, KC_P2, KC_P3, _______, _______, _______, _______,  _______, _______, _______, _______,  KC_P1, KC_P2, KC_P3, _______,
-      _______, KC_P0, KC_PDOT, KC_PENT, _______,     _______,  _______,   _______,    _______,   _______,     KC_P0, KC_PDOT, KC_PENT, _______
+      KC_ESC,    KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_BSPC,
+      _______,   _______,     _______,      _______,   _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
+      KC_CAPS,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
+      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 
 	[3] = LAYOUT(
-      QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-      _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, KC_F12,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_SPI, RGB_HUI, RGB_SAI, RGB_VAI,
-      _______, _______, _______, _______, _______,      _______,     _______, _______,     _______,      RGB_RMOD, RGB_SPD, RGB_HUD, RGB_SAD, RGB_VAD
+      QK_BOOT,   KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_F11,
+      _______,   _______,     _______,      _______,   _______,    _______,                                                          QK_BOOT,    _______,     _______,     _______,    _______,    KC_F12,
+      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    RGB_TOG,     _______,     _______,    _______,    _______,
+      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    RGB_MOD,     RGB_SPI,     RGB_HUI,    RGB_SAI,    RGB_VAI,
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         _______,          _______,           _______,           RGB_RMOD,    RGB_SPD,     RGB_HUD,    RGB_SAD,    RGB_VAD
       )
 
 };

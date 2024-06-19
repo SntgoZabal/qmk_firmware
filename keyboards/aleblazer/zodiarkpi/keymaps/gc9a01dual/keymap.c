08823961850f32ee6265f1de15d1cce4c3628c60
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-06-18 22:04:51 -0600

    Update keymap.c

--- keyboards/aleblazer/zodiarkpi/keymaps/gc9a01dual/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/gc9a01dual/keymap.c
@@ -110,9 +110,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
       KC_ESC,    KC_1,        KC_2,         KC_3,      KC_4,       KC_5,                                                             KC_6,       KC_7,        KC_8,        KC_9,       KC_0,       KC_BSPC,
       KC_TAB,    KC_Q,        KC_W,         KC_E,      KC_R,       KC_T,                                                             KC_Y,       KC_U,        KC_I,        KC_O,       KC_P,       KC_LBRC,
-      KC_MPLY,   KC_A,        KC_S,         KC_D,      KC_F,       KC_G,     KC_RSFT,    KC_MUTE,          RGB_TOG,     KC_ENT,      KC_H,       KC_J,        KC_K,        KC_L,       KC_SCLN,    KC_RBRC,
+      KC_MPLY,   KC_A,        KC_S,         KC_D,      KC_F,       KC_G,     KC_ENT,     KC_MUTE,          RGB_TOG,     KC_ENT,      KC_H,       KC_J,        KC_K,        KC_L,       KC_SCLN,    KC_RBRC,
       KC_LSFT,   KC_Z,        KC_X,         KC_C,      KC_V,       KC_B,     KC_LCTL,    KC_LALT,          KC_EQL,      KC_MINS,     KC_N,       KC_M,        KC_COMM,     KC_DOT,     KC_SLSH,    KC_QUOT,
-      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  KB_ACCENT,          KC_SPC,          KC_SLEP,          KC_LGUI,           KC_SPC,            KC_LEFT,     KC_UP,       KC_DOWN,    KC_RGHT,    KC_BSLS
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  KB_ACCENT,       KC_SPC,          KC_LGUI,          KC_LGUI,           KC_SPC,            KC_LEFT,     KC_UP,       KC_DOWN,    KC_RGHT,    KC_BSLS
     ),
 
 	[1] = LAYOUT(
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_PSCR,   _______,     _______,      _______,   _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
       KC_CAPS,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
       _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
-      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         KC_SLEP,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 
 	[2] = LAYOUT(
@@ -128,13 +128,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_PSCR,   _______,     _______,      _______,   _______,    _______,                                                          KC_NUM,     KC_P7,       KC_P8,       KC_P9,      _______,    KC_F11,   
       KC_CAPS,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     KC_PSLS,    KC_P4,       KC_P5,       KC_P6,      _______,    KC_F12,
       _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    KC_P1,       KC_P2,       KC_P3,      _______,    _______,
-      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         _______,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         KC_SLEP,          _______,           _______,           KC_P0,       KC_PDOT,     KC_PENT,    _______,    _______
       ),
 
 	[3] = LAYOUT(
-      QK_BOOT,   KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_F11,
-      _______,   _______,     _______,      _______,   _______,    _______,                                                          QK_BOOT,    _______,     _______,     _______,    _______,    KC_F12,
-      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    RGB_TOG,     _______,     _______,    _______,    _______,
+      QK_BOOT,   KC_F1,       KC_F2,        KC_F3,     KC_F4,      KC_F5,                                                            KC_F6,      KC_F7,       KC_F8,       KC_F9,      KC_F10,     KC_BSPC,
+      _______,   _______,     _______,      _______,   _______,    _______,                                                          QK_BOOT,    _______,     _______,     _______,    _______,    KC_F11,
+      _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    RGB_TOG,     _______,     _______,    _______,    KC_F12,
       _______,   _______,     _______,      _______,   _______,    _______,  _______,    _______,          _______,     _______,     _______,    RGB_MOD,     RGB_SPI,     RGB_HUI,    RGB_SAI,    RGB_VAI,
       KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  _______,         _______,         _______,          _______,           _______,           RGB_RMOD,    RGB_SPD,     RGB_HUD,    RGB_SAD,    RGB_VAD
       )

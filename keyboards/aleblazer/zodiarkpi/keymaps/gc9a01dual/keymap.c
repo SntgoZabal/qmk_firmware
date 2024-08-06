c1bbbdee3ba77c7fcf44b1314ea134877165658b
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-08-06 10:48:18 -0600

    Update keymap.c

--- keyboards/aleblazer/zodiarkpi/keymaps/gc9a01dual/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/gc9a01dual/keymap.c
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,    KC_Q,        KC_W,         KC_E,      KC_R,       KC_T,                                                             KC_Y,       KC_U,        KC_I,        KC_O,       KC_P,       KC_LBRC,
       KC_MPLY,   KC_A,        KC_S,         KC_D,      KC_F,       KC_G,     KC_ENT,     KC_MUTE,          RGB_TOG,     KC_ENT,      KC_H,       KC_J,        KC_K,        KC_L,       KC_SCLN,    KC_RBRC,
       KC_LSFT,   KC_Z,        KC_X,         KC_C,      KC_V,       KC_B,     KC_LCTL,    KC_LALT,          KC_EQL,      KC_MINS,     KC_N,       KC_M,        KC_COMM,     KC_DOT,     KC_SLSH,    KC_GRAVE,
-      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  KB_QUOT,       KC_SPC,          KC_LGUI,          KC_LGUI,           KC_SPC,            KC_LEFT,     KC_UP,       KC_DOWN,    KC_RGHT,    KC_BSLS
+      KB_MODE0,  KB_MODE1,    KB_MODE2,     KB_MODE3,  KB_QUOTE,       KC_SPC,          KC_LGUI,          KC_LGUI,           KC_SPC,            KC_LEFT,     KC_UP,       KC_DOWN,    KC_RGHT,    KC_BSLS
     ),
 
 	[1] = LAYOUT(

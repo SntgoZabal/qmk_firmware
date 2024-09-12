f29749cadabd809e2af43daf1b5d86243efca8bb
SntgoZabal <167109202+SntgoZabal@users.noreply.github.com>
2024-09-12 22:48:24 +0100

    Update keymap.c

--- keyboards/aleblazer/zodiarkpi/keymaps/gc9a01cirque/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/gc9a01cirque/keymap.c
@@ -244,7 +244,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void pointing_device_init_user(void) {
-//  set_auto_mouse_layer(<mouse_layer>); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
+    set_auto_mouse_layer(3); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
 }
 
